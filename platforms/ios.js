a8210bb0c06a6d9b9f18c4c68ce882089b2b66a2
Cesidio DiBenedetto <sid.dibenedetto@gmail.com>
2014-10-24 16:34:48 -0400

    fixed splashScreenNameList for Portrait@2x~ipad

--- platforms/ios.js
+++ platforms/ios.js
@@ -10,9 +10,8 @@
         iconNameList = ['', '@2x', '-40', '-40@2x', '-50', '-50@2x', '-60', '-60@2x', '-72', '-72@2x', '-76', '-76@2x', '-small', '-small@2x'],
         iconDimensions = [57, 114, 40, 80, 50, 100, 60, 120, 72, 144, 76, 152, 29, 58],
         numOfIcons = iconNameList.length,
-        splashScreenNameList = ['~iphone', '@2x~iphone', '-Portrait~ipad', '-@2x~ipad', '-Landscape~ipad', '-Landscape@2x~ipad', '-568h@2x~iphone'],
+        splashScreenNameList = ['~iphone', '@2x~iphone', '-Portrait~ipad', '-Portrait@2x~ipad', '-Landscape~ipad', '-Landscape@2x~ipad', '-568h@2x~iphone'],
         splashScreenDimensions = ['320x480', '640x960', '784x1024', '1536x2048', '1024x768', '2048x1536', '640x1136'];
-
 
     function _generateIcons(input, cb) {
         var wstream = fs.createWriteStream(mainDirectory+'ios.xml');
